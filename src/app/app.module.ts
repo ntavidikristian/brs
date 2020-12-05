2fa9b674348bf937d5c65781d0f72bf8976c235a
Eleni Karagiannidou <elenikara1997@gmail.com>
2020-12-05 12:08:49 +0200

    Adding nav-bar, font-awesome, styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BugsListComponent } from './bugs-list/bugs-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BugsListComponent
+    BugsListComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
