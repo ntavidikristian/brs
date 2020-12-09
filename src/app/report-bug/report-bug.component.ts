dde0d57756b393e8062af3c7dbfabcab4dec1e70
Eleni Karagiannidou <elenikara1997@gmail.com>
2020-12-09 22:01:40 +0200

    User case 2 semi finished

--- src/app/report-bug/report-bug.component.ts
+++ src/app/report-bug/report-bug.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { BugInterface } from '../bug-interface';
+import { RestService } from '../rest.service';
 @Component({
   selector: 'app-report-bug',
   templateUrl: './report-bug.component.html',
   styleUrls: ['./report-bug.component.scss']
 })
 export class ReportBugComponent implements OnInit {
+  reportBugForm: FormGroup;
 
-  constructor() { }
+  constructor(private service : RestService) { }
 
   ngOnInit(): void {
+    this.reportBugForm = new FormGroup({
+      bugTitle: new FormControl("",[Validators.required]),
+      bugDescription: new FormControl("",[Validators.required]),
+      bugPriority: new FormControl("",[Validators.required]),
+      bugReporter: new FormControl("",[Validators.required]),
+      bugStatus: new FormControl("")
+    });
+    this.reportBugForm.get("bugReporter").statusChanges.subscribe((value) => {
+      console.log(value);
+      if(this.reportBugForm.get("bugReporter").value == '1'){
+        this.reportBugForm.get("bugStatus").setValidators(Validators.required)
+      }else{
+        this.reportBugForm.get("bugStatus").clearValidators();
+      }
+      this.reportBugForm.get("bugStatus").updateValueAndValidity();
+    })
+  }
+
+  onSubmit(){
+    if(!this.reportBugForm.valid){
+      console.log('invalid');
+    }else{
+      console.log(this.reportBugForm);
+      let values = this.reportBugForm.value;
+      let sendingBug: BugInterface ={
+        title : values.bugTitle,
+        description : values.bugDescription,
+        priority : values.bugPriority,
+        reporter : values.bugReporter,
+        status : values.bugStatus
+      };
+      // sendingBug.title = values.bugTitle;
+      // sendingBug.description = values.bugDescription;
+      // sendingBug.priority = values.bugPriority;
+      // sendingBug.reporter = values.bugReporter;
+      // sendingBug.status = values.bugStatus;
+      console.log(sendingBug);
+      // this.service.postBug(sendingBug).subscribe((data)=>{
+      //   console.log(data);
+      // });
+    }
+    
   }
 
 }
+
+
